d0c7f42fbe5b4610688effb60ac073d25e36d04a
Eric Syme <spotlightphotoseattle@gmail.com>
2019-12-17 21:41:04 -0800

    first commit frameworks

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle} from 'reactstrap';
+import { FadeTransform } from 'react-animation-components';
+import { Link } from 'react-router-dom';
+
+
+
+function RenderCard({item}) {
+        return(
+            <FadeTransform in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)' 
+                }}>
+                <Card>
+                    <CardImg height="175px" src={item.image} alt={item.name} />
+                    <CardBody>
+                    <CardTitle id="black">{item.name}</CardTitle>
+                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+                    <CardText id="black">{item.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
+        );
+
+}
+
+function Home(props) {
+    return(
+      <div className="container">
+        <div className="row">
+            <h1 className="mx-auto"><strong>Hurricane Ridge Ski and Snowboard Area</strong></h1>
+        </div>
+        <div className="row align-items-start home">
+            <div className="col-12 col-md m-1">
+                <h3>PROJECTS</h3>
+                <Link to="/projects">
+                <RenderCard item={props.project}/>
+                </Link>
+            </div>
+            <div className="col-12 col-md m-1">
+                <h3>CERTIFICATES</h3>
+                <Link to="/certificates">
+                <RenderCard item={props.certificate} />
+                </Link>
+            </div>
+            <div className="col-12 col-md m-1">
+                <h3>BIOGRAPHY</h3>
+                <Link to="/biography">
+                <RenderCard item={props.biography} />
+                </Link>
+            </div>
+        </div>
+      </div>
+    );
+}
+
+export default Home; 
