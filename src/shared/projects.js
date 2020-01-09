c7e90f3289c9fb9c6f4ff3b217659e0fc29c4c91
Eric Syme <spotlightphotoseattle@gmail.com>
2020-01-09 11:13:37 -0800

    starting array and table data

--- src/shared/projects.js
+++ src/shared/projects.js
@@ -10,21 +10,19 @@ export const PROJECTS =
         },
         {
             id: 1,
-            name:'Skiing & Riding',
-            image: '/assets/images/ridgemap.jpg',
+            name:'Day Rates and Passes',
+            image: '/assets/images/snowboarding.jpg',
             category: 'mains',
             featured: false,
-            sandbox: "https://codesandbox.io/embed/myweatherapp-l5jly?fontsize=14",
-            description:'Hurricane Ridge Ski and Snowboard Area is located in the Olympic National Park, 17 miles south of Port Angeles, Washington, one of only two remaining lift operations in the National Parks. Rising a mile high, Hurricane Ridge offers winter recreation and activities and features winter vistas unmatched anywhere in the Pacific Northwest. It is a small, family oriented ski area, offering to residents and visitors alike a quality winter sports experience, without the high cost or congestion of most ski areas.  The Ridge boasts some groomed areas, but for the accomplished skier or snowboarder the steeps, bowls and glades are well worth the effort it takes to get there.  With a summit elevation of 5,240 feet, the average annual snowfall is 400-plus inches. LIFTS -- We have 3 surface lifts at Hurricane Ridge: "The Bunny" our beginner row tow lift, "The Intermediate" rope tow lift, and "The Poma" platter lift.'
+            description:'An updated and Reactified version of the Bootstrap project, this React.js app operates as a single-page application using Redux, Thunk, and Fetch to use with a JSON server to hold data that is changed by the user.  Similarly, this portfolio site offers the ability to add to the comments of each project.  Go ahead, add a comment by clicking the button to the right. '
         },
         {
             id: 2,
-            name:'React -- Ristorante ConFusion',
-            image: '/assets/images/reactConfusion.png',
+            name:'Skiing & Riding Area Map',
+            image: '/assets/images/ridgemap2.jpg',
             category: 'mains',
             featured: false,
-            sandbox: "https://codesandbox.io/embed/github/EricSyme/React-ConFusion/tree/master/?fontsize=14",
-            description:'An updated and Reactified version of the Bootstrap project, this React.js app operates as a single-page application using Redux, Thunk, and Fetch to use with a JSON server to hold data that is changed by the user.  Similarly, this portfolio site offers the ability to add to the comments of each project.  Go ahead, add a comment by clicking the button to the right. '
+            description:'Hurricane Ridge Ski and Snowboard Area is located in the Olympic National Park, 17 miles south of Port Angeles, Washington, one of only two remaining lift operations in the National Parks. Rising a mile high, Hurricane Ridge offers winter recreation and activities and features winter vistas unmatched anywhere in the Pacific Northwest. It is a small, family oriented ski area, offering to residents and visitors alike a quality winter sports experience, without the high cost or congestion of most ski areas.  The Ridge boasts some groomed areas, but for the accomplished skier or snowboarder the steeps, bowls and glades are well worth the effort it takes to get there.  With a summit elevation of 5,240 feet, the average annual snowfall is 400-plus inches. LIFTS -- We have 3 surface lifts at Hurricane Ridge: "The Bunny" our beginner row tow lift, "The Intermediate" rope tow lift, and "The Poma" platter lift.'
         },
         {
             id: 3,
